643ab1734d9a9e3bae28ef73608a3c54c3ccd47c
Thor Even Tutturen <t.tutturen@gmail.com>
2015-11-16 14:13:18 +0100

    simplified HextechSolver

--- src/hextech/HextechSolver.java
+++ src/hextech/HextechSolver.java
@@ -2,7 +2,6 @@ package hextech;
 
 import org.sat4j.minisat.SolverFactory;
 import org.sat4j.specs.ContradictionException;
-import org.sat4j.specs.IProblem;
 import org.sat4j.specs.ISolver;
 import org.sat4j.specs.TimeoutException;
 
@@ -15,29 +14,19 @@ public class HextechSolver {
     }
 
     public Boolean ask(KnowledgeBase base, int maxVar) {
-        final int NBCLAUSES = base.size();
         solver.reset();
         solver.newVar(maxVar);
-        solver.setExpectedNumberOfClauses(NBCLAUSES);
-        for (int i = NBCLAUSES - 1; i >= 0; i--) {
+        solver.setExpectedNumberOfClauses(base.size());
+        for (int i = base.size() - 1; i >= 0; i--) {
             try {
                 solver.addClause(base.getVecInt(i));
             } catch (ContradictionException e) {
-                //System.out.println(e.getMessage());
-                //System.out.println("CONTRADICTION");
                 return null;
             }
         }
 
-        IProblem problem = solver;
         try {
-            if(problem.isSatisfiable()) {
-                //System.out.println("Satisfiable");
-                return true;
-            } else {
-                //System.out.println("Not satisfiable");
-                return false;
-            }
+            return solver.isSatisfiable();
         } catch (TimeoutException e) {
             e.printStackTrace();
         }
